21759214cd0311969fb48817cd0fa8f35df89a90
Aaro Harju <aaro,.m.harju@gmail.com>
2022-03-05 14:33:08 +0200

    first commit

---
+++ dictionary.js
@@ -0,0 +1,308 @@
+let words = [
+  [
+    "pear",
+    "päärynä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStvLIxHVZjLicLZxNlXbrZZyTws68nGEROC-KbbNWDEHZUc_oOom9wqrkqxBqa0ZJ24nA&usqp=CAU",
+  ],
+  [
+    "sun",
+    "aurinko",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd72499NH8cAwb9TBuK2flhQuzsab7qO4z8Q&usqp=CAU",
+  ],
+  [
+    "car",
+    "auto",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMHGxIEsSRVxaTHQDGkxlZWseUVx5cTu6Y8Q&usqp=CAU",
+  ],
+  [
+    "house",
+    "talo",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkF3JZlgEJ6mNAWuyr_3c8XTmYQh-rM_n5XQ&usqp=CAU",
+  ],
+  [
+    "family",
+    "perhe",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEL7VhH5qdgUX8eXfsFmSnvhn2js050CLRrQ&usqp=CAU",
+  ],
+  [
+    "moon",
+    "kuu",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaKm1n_JrmoinyBp-9gf2zCOwt1l_bvNBJyA&usqp=CAU",
+  ],
+  [
+    "summer",
+    "kesä",
+    "https://www.uniteddigitallearning.com/wp-content/uploads/2018/08/summer-4-1080x675.jpg",
+  ],
+  [
+    "winter",
+    "talvi",
+    "https://media.istockphoto.com/videos/winter-and-christmas-background-with-snowflakes-falling-animation-video-id1285402578?s=640x640",
+  ],
+  [
+    "holiday",
+    "loma",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKzyebenu9BiV50PgJKIOjdOe0Nfop6YAoPQ&usqp=CAU",
+  ],
+  [
+    "dog",
+    "koira",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrAw6VrCC3nbntwKGxaX7uHZ4zW3UQnZmmvA&usqp=CAU",
+  ],
+  [
+    "rabbit",
+    "jänis",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpTHuA-Vl1oRO84wJOWXBxJMeM70v4cQTADQ&usqp=CAU",
+  ],
+  [
+    "fox",
+    "kettu",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZX_myYDdbKp1lO9M1TUarCjBszWY_Zphu-w&usqp=CAU",
+  ],
+  [
+    "elephant",
+    "elefantti",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLZym5lb0VLXbNLPhWl_H4GlRAhkSg1Ju16A&usqp=CAU",
+  ],
+  [
+    "autumn",
+    "syksy",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG4fOh9j-5llqdzYVzYFkGvutYOn7w5IRArQ&usqp=CAU",
+  ],
+  [
+    "spring",
+    "kevät",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtzcg8YSmEO6w0fj3gwlIduCUlPf6ZReevhw&usqp=CAU",
+  ],
+  [
+    "shoe",
+    "kenkä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfxmkwV1j4439mAl7_VXts3dhiv2m-3FQ94Q&usqp=CAU",
+  ],
+  [
+    "hat",
+    "hattu",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZxHCRcYuYdfYsNZukh_nX3ypVsXSJvhe-9w&usqp=CAU",
+  ],
+  [
+    "skirt",
+    "hame",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt4EvZxL_5z9s1P7JKK6tqn2QYlCRewsYaJA&usqp=CAU",
+  ],
+  [
+    "mother",
+    "äiti",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP51olFTXSuFo9RKh7YWxtEbXzoY_YLcnhpQ&usqp=CAU",
+  ],
+  [
+    "father",
+    "isä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi07daYqX4UgQPDpxYCB2JVMMfg91xbO17KA&usqp=CAU",
+  ],
+  [
+    "girl",
+    "tyttö",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9MC6QYBSg0uYurg5shUywTaIGjXYi750QGw&usqp=CAU",
+  ],
+  [
+    "boy",
+    "poika",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdVg34rLrZ7TJ0Oqvn7Q8o9WtnqJV56SUrRg&usqp=CAU",
+  ],
+  [
+    "aunt",
+    "täti",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW5q_rpSOnYeCttCVgYyHaQg6cO5SQJzRXwA&usqp=CAU",
+  ],
+  [
+    "uncle",
+    "setä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9urRqmFxwwoOjfEVziGGzl39bMt4ZVbGs6A&usqp=CAU",
+  ],
+  [
+    "flower",
+    "kukka",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSAIYm-QGaGXg5DT5v9oyOespjuwHqw0--w&usqp=CAU",
+  ],
+  [
+    "mountain",
+    "vuori",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFI1gHDbvj9HZlSn-Wf7fZuTjFiIw0mCU33Q&usqp=CAU",
+  ],
+
+  [
+    "tree",
+    "puu",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSAT3gDmE6ZqiuUyvbOLtO7713N1Jr5C0S7A&usqp=CAU",
+  ],
+  [
+    "coffee",
+    "kahvi",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjjm3F0JgReyz7GErGxO-Vbod8TShmMc6Gag&usqp=CAU",
+  ],
+  [
+    "chocolate",
+    "suklaa",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhyf4Z1xe2f5LFTUe4Rb92iJ2AmYSBc90pRA&usqp=CAU",
+  ],
+  [
+    "cake",
+    "kakku",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5P8HUBJgHYXx4K0bW6gVTThRnizPstk-Vw&usqp=CAU",
+  ],
+  [
+    "ice cream",
+    "jäätelö",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgEHJ-vyh9xptTckoi3yCqdr8HWWBxXIUL6A&usqp=CAU",
+  ],
+  [
+    "apple",
+    "omena",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqxc7JwLYbYXTJEiezLyw0NA6Hgdoiiu4wYA&usqp=CAU",
+  ],
+  [
+    "banana",
+    "banaani",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa0gcnXu_Y0kMAJ40cJ_qcilUAngqjYk6xhA&usqp=CAU",
+  ],
+  [
+    "horse",
+    "hevonen",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEYhR31dJsFvUM07SYYKrWVQD4VCMQd039fQ&usqp=CAU",
+  ],
+  [
+    "cow",
+    "lehmä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyScnphEg-hCRRdl-2-a1s2jXl5fgVSAIMMQ&usqp=CAU",
+  ],
+  [
+    "sheep",
+    "lammas",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHlU6lHgOdcBzl1zlMz_gKkztuxM-ydQ_oJg&usqp=CAU",
+  ],
+  [
+    "teacher",
+    "opettaja",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4bX9Z6Dv2rJqYRF8GY6_rN_QRVhnVjenkEw&usqp=CAU",
+  ],
+  [
+    "police",
+    "poliisi",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq6UzgXARvwSQbmST8Tj0Sl9wRbcijVOX7rQ&usqp=CAU",
+  ],
+  [
+    "pilot",
+    "lentäjä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb4UFVj_3M4LUrGSz-nLfRI5uxh5mKnTMZmg&usqp=CAU",
+  ],
+  [
+    "nurse",
+    "sairaanhoitaja",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMDBndJALINfKsf5jpfGL-q7ARLepurlDmqw&usqp=CAU",
+  ],
+  [
+    "train",
+    "juna",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsK1_aUupVdut262hNGkZkqV_giSFJqtPrRQ&usqp=CAU",
+  ],
+  [
+    "ship",
+    "laiva",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxwIWUm22HeUWRBaFFdqRrsnnQgPiV4DuGtg&usqp=CAU",
+  ],
+  [
+    "airplane",
+    "lentokone",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNc9zUaw2Qq9E7Kw1ukEhccVsib1Whdw-TXg&usqp=CAU",
+  ],
+  [
+    "farm",
+    "maatila",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX4Ou5zu6TKIg_aLBsikrlOLMgEcAonihJQA&usqp=CAU",
+  ],
+  [
+    "bicycle",
+    "polkupyörä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI9Lat0pHmqzUszsjuj_ETugdUHDcbcIYlSg&usqp=CAU",
+  ],
+  [
+    "motorcycle",
+    "moottoripyörä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE8DCJERBR8IRsaYlbOG4JCQyi2JfD9DTFjA&usqp=CAU",
+  ],
+  [
+    "book",
+    "kirja",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTGaQB3N1EsPSDZYbAJi2n9aov3DrkGXs60g&usqp=CAU",
+  ],
+  [
+    "pen",
+    "kynä",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvsBSZtOFqdOc3QUFB9vD-L13QWWpuEtqMNA&usqp=CAU",
+  ],
+  [
+    "reindeer",
+    "poro",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR6Ky0zuVTcWdYzvp3SyGlZU_K94OWDQ5hnQ&usqp=CAU",
+  ],
+  [
+    "sauna",
+    "sauna",
+    "https://media.istockphoto.com/vectors/cartoon-color-characters-people-and-sauna-bathing-concept-vector-vector-id1303900041?k=20&m=1303900041&s=612x612&w=0&h=OHz6POxmImGw_-DDfb-i2DkZ1vGR2VnFhoNf-mizebY=",
+  ],
+];
+
+init = function () {
+  for (let i = 0; i < words.length; i++) {
+    const innerArray = words[i].length;
+    for (let j = 0; j < innerArray; j++) {
+      if (j == 0) {
+        document.getElementById("word_list").innerHTML +=
+          "<li onclick='show(" + i + ")'>" + words[i][j] + "</li>";
+      }
+    }
+  }
+};
+init();
+
+search = function () {
+  query = document.getElementById("search").value;
+
+  if (query == "") {
+    return;
+  }
+
+  found = -1;
+  let index_1 = 0;
+  let index_2 = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    const innerArray = words[i].length;
+    for (let j = 0; j < innerArray; j++) {
+      if (query == words[i][j] && j == 0) {
+        found = 0;
+        index_1 = i;
+        index_2 = j;
+        break;
+      } else {
+        document.getElementById("word_eng").innerHTML =
+          "Word not found, please select another word.";
+        document.getElementById("word_fin").innerHTML =
+          "Translation not found!";
+        document.getElementById("image").innerHTML = "No image found!";
+        document.getElementById("photo-credit").href="#"
+      }
+    }
+  }
+  if (found == 0) {
+    show(index_1, index_2);
+  }
+};
+
+show = function (index_1, index_2 = 0) {
+  document.getElementById("word_eng").innerHTML = words[index_1][index_2];
+  document.getElementById("word_fin").innerHTML = words[index_1][index_2 + 1];
+  document.getElementById("image").src = words[index_1][index_2 + 2];
+  document.getElementById("photo-credit").href=words[index_1][index_2 + 2];
+  document.getElementById("image-fin").style.display = "none";
+};
